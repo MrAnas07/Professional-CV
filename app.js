325fd1ce6fca9d0e47b06afcacdb6b1e678dde30
Mr Anas <154543858+MrAnas07@users.noreply.github.com>
2024-02-21 19:54:14 +0500

    Professional CV Using Javascript
    Professional CV Using Javascript

---
+++ app.js
@@ -0,0 +1,29 @@
+document.write("<center><h1>Professional CV</h1></center><br>")
+document.write("<center><h2>Name: Muhammad Anas</h2></center>")
+document.write("<center><h2>Addrees: Liqutabad Karachi </h2></center>")
+document.write("<center><h2>Email: anusamir45gamil.com </h2></center>")
+document.write("<center><h2>Contact: 03126--107 </h2></center>")
+document.write("<h1>Objective</h1>")
+document.write("<p>To seek a challenging position in a repotable progressive and growing organization.</p>")
+document.write("<h1>Qualification</h1>")
+document.write("<p>Matric O/Level</p>")
+document.write("<h1>Skills</h1>")
+document.write("<p><li>Digital Marketing</li></p>")
+document.write("<p><li>Web Development</li></p>")
+document.write("<p><li>Artificial Intelligence</li></p>")
+document.write("<p><li>Blockchain</li></p>")
+document.write("<h1>Work Experience</h1>")
+document.write("<p>Technology Fields / Crypto Trading Almost 2 years </p>")
+document.write("<h1>Personal Information </h1>")
+document.write("<p>Name: Muhammad Anas </p>")
+document.write("<p>F/Name: Muhammad Amir </p>")
+document.write("<p>Contact: 03126--107 </p>")
+document.write("<p>Email: anusamir45@gamil.com </p>")
+document.write("<h1>Reference</h1>")
+document.write("<p>Will be furnish on request </p>")
+
+
+
+
+
+
